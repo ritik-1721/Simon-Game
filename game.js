3888a30d41945ff7d92a585f695c8e6fd0756795
Ritik pawar <83938643+ritik-1721@users.noreply.github.com>
2021-09-10 15:58:51 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,86 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if(!started) {
+    nextSequence();
+    console.log("run");
+    started = true;
+  }
+});
+
+
+function nextSequence(){
+  level++;
+  $("#level-title").text("Level " + level);
+
+  console.log(level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  //console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      //console.log("working");
+       setTimeout(function () {
+        nextSequence();
+        }, 1000);
+    }
+  }
+
+  else {
+    playSound("wrong");
+    //console.log("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any key to Restart");
+
+    setTimeout(function () {
+     $("body").removeClass("game-over");
+      }, 200);
+
+    startOver();
+  }
+
+}
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+   }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern= [];
+  started = false;
+}
